6353d0f07d74afa63aef33436123fdbdb1b1cab1
Joy-whale0321 <jingyuzhang21@m.fudan.edu.cn>
2025-11-24 13:54:35 +0800

    Create MergeBash.sh

---
+++ HF_PY/Generate/DataSet/MergeBash.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 出错就退出
+set -e
+
+# 输出文件名你可以按需要改
+OUT0="ppHF_eXDecay_p5B_1.root"
+OUT1="ppHF_eXDecay_p5B_2.root"
+
+# 合并 0-499
+echo "Merging 0-499 -> ${OUT0}"
+hadd -f "${OUT0}" ppHF_eXDecay_1M_{0..499}.root
+
+# 合并 500-999
+echo "Merging 500-999 -> ${OUT1}"
+hadd -f "${OUT1}" ppHF_eXDecay_1M_{500..999}.root
+
+echo "Done."
